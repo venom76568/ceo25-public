1079f886f4c0351a7895102e0d8c0519717e3973
Saransh Dwivedi <saranshdwivedi08@gmail.com>
2024-01-09 23:43:58 +0530

    change in app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React from "react";
 import {BrowserRouter} from "react-router-dom";
 import Hero from "./components/Hero1";
@@ -10,23 +10,6 @@ function App() {
       <Hero/>
       <AboutSection/>
     </BrowserRouter>
-=======
-import logo from "./logo.svg";
-import "./App.css";
-import About from "./components/About";
-import Hero from "./components/Hero";
-import Timeline from "./components/Timeline1";
-// import Timeline1 from './components/Time';
-
-function App() {
-  return (
-    <div className="App">
-      <Hero />
-      <About />
-      <Timeline />
-      {/* <Timeline1/> */}
-    </div>
->>>>>>> 6107dae01eae3ae86a6eb050d0eb8b91414b64c2
   );
 };
 
