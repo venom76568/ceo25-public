d0e3b3f85e92d40a1b5ce0a91d913e7bc9193751
Sanchit Padwekar <86397529+soulfaced@users.noreply.github.com>
2024-01-09 23:34:50 +0530

    timeline added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import About from './components/About';
 import Hero from './components/Hero';
+import Timeline from './components/Timeline1';
+// import Timeline1 from './components/Time';
 
 function App() {
   return (
     <div className="App">
       <Hero/>
       <About />
+      <Timeline />
+      {/* <Timeline1/> */}
     </div>
   );
 }
