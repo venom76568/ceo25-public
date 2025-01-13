2e2973e1e905720d3acd5c558ead148c967531ad
honeypal36 <connect.honeypal@gmail.com>
2025-01-13 19:22:17 +0530

    Update Form1.jsx

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -15,6 +15,7 @@ const SignUpForm = () => {
     phone: "",
     email: "",
     box: "",
+    college: "",
     question1: "",
     question2: "",
     question3: "",
@@ -151,6 +152,27 @@ const SignUpForm = () => {
           <span style={{ color: "red" }}>{errors.email}</span>
           <br />
         </div>
+        <div>
+            <label className="block mb-2 text-sm font-medium text-white">
+              College Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Your College Name Here"
+              // pattern={field.pattern}
+              required
+              // value={formData.field.name}
+              // value={formData[field.name]}
+              // onChange={handleChange}
+              name="college"
+              value={formData.college}
+              onChange={handleChange}
+            />
+            <span style={{ color: "red" }}>{errors.college}</span>
+            <br />
+          </div>
         {currentStep === 2 && (
           <>
             {/* Additional fields for the second step */}
