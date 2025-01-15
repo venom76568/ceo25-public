8636aa505ec68df76bba8f9e0191f3d711cd007a
honeypal36 <connect.honeypal@gmail.com>
2025-01-15 21:26:29 +0530

    Update Hero1.jsx

--- src/components/Hero1.jsx
+++ src/components/Hero1.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
         <div className="justify-center items-center text-center flex flex-row">
           <div className="flex flex-col w-full sm:w-auto sm:flex-row p-4">
             <a
-              href="#register"
+              href="https://shorturl.at/yHIyQ"
               className="flex flex-row w-[250px] self-center items-center justify-center px-4 py-4 mb-4 text-xl text-white font-sans bg-sky-600 leading-6 capitalize duration-100 transform rounded-sm shadow cursor-pointer focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50 focus:outline-none sm:mb-0 sm:w-auto sm:mr-4 md:pl-8 md:pr-6 xl:pl-12 xl:pr-10   hover:shadow-lg hover:-translate-y-1 rounded-lg"
             >
               Register
